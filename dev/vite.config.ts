daa63911cc7a5782d5a6f139596f1c31faec7f3f
Benji Taylor <30378142+benjitaylor@users.noreply.github.com>
2026-02-23 16:36:44 -0800

    Clean up dev config

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, 'index.html'),
         demo: path.resolve(__dirname, 'demo.html'),
-        showcase: path.resolve(__dirname, 'showcase.html'),
       },
     },
   },
